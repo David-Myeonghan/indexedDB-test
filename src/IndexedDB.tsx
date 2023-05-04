06dbf55599ab1efddd0e9e40796866744864a56c
davidRyu <david.ryu@imagoworks.ai>
2023-05-04 18:19:25 +0900

    style: add comment

--- src/IndexedDB.tsx
+++ src/IndexedDB.tsx
@@ -5,7 +5,8 @@ const IndexedDB = () => {
 
   useEffect(() => {
     console.log("useEffect");
-    console.log(window.indexedDB);
+
+    // 1. Check if 'window.indexedDB' exist.
     if (!window.indexedDB) {
       window.alert(
         "Your browser doesnt support a stable version of IndexedDB. Such and such feature will not be available"
@@ -16,60 +17,79 @@ const IndexedDB = () => {
     const version = 1;
     let db = null;
 
-    // IDBOpenDBRequest
-    const request = indexedDB.open(name, version);
+    // 2. Open DB with 'indexedDB.open()' and returns IDBOpenDBRequest.
+    const request = indexedDB.open(name, version); // IDBOpenDBRequest
     console.log(request);
 
-    // trigger when db is loaded with bigger version than existing number.
-    request.onupgradeneeded = function (event) {
-      // IDBDatabase
-      db = request.result;
+    // 3. As DB is initialised first time, 'onupgradedneeded()' is called.
+    request.onupgradeneeded = function (event) { // trigger when db is loaded with bigger version than existing number.
+      // 4. get IDBDatabase as a result of request.
+      db = request.result; // IDBDatabase
       console.log(db);
 
       const key = "id";
       const name = "store name"; // store name
 
-      // IDBObjectStore
-      const store = db.createObjectStore(name, { keyPath: key });
+      // 5. Create object store to store objects, returns IDBObjectStore.
+      const store = db.createObjectStore(name, { keyPath: key }); // IDBObjectStore
       console.log(store);
 
-      const indexName = "by_name"; // table name
       const keyPath = "name";
+      const indexName = "by_name"; // table name
 
-      // IDBIndex
-      const index = store.createIndex(indexName, keyPath);
+      // 6. Create 'Index' object store to store 'Index' objects, returns IDBIndex.
+      const index = store.createIndex(indexName, keyPath); // IDBIndex
       console.log(index);
 
       const obj = { [key]: 1, [keyPath]: "name" };
+      // 7. Add a record with IDBObjectStore.
       store.put(obj);
     };
+
+    // 8. As DB is called successfully, onsuccess callback is invoked.
+    // (not because of onupgradeneeded or store.pur(), but because of .open() successfully)
     request.onsuccess = function (event) {
       db = request.result;
       console.log(db);
 
-      // IDBTransaction
-      const transaction = db.transaction(["store name"], "readonly");
+      // 9. Create transaction context with transaction and returns IDBTransaction.
+      const transaction = db.transaction(["store name"], "readonly"); // IDBTransaction
 
-      // IDBObjectStore
-      const objectStore = transaction.objectStore("store name");
+      // 10. Find object store with store name, and it returns IDBObjectStore.
+      const objectStore = transaction.objectStore("store name"); // IDBObjectStore
 
-      // IDBRequest
-      const cursor = objectStore.openCursor();
+      // 11. get IDBRequest using 'openCursor' that requests Cursor.
+      const cursor = objectStore.openCursor(); // IDBRequest
+      console.log(cursor)
+
+      // 12. As Cursor request is successful, onsuccess is invoked.
       cursor.onsuccess = function (event: Event) {
-        // IDBCursorWithValue
         console.log(event);
+        // 13. Get IDBCursorWithValue as result of event..?
         const target = event.target as IDBRequest;
+
+        // 14. Find value contained in cursor.
         const cursor = target.result ?? "";
-        console.log(cursor)
+        console.log(cursor);
         if (cursor) {
-            // { id: 1, name: 'name' }
-            console.log(cursor.value);
-            cursor.continue();
+          // { id: 1, name: 'name' }
+          console.log(cursor.value);
+
+          // 15. Call next cursor using 'continue'
+          cursor.continue();
         } else {
-            console.log('end');
+          console.log("end");
         }
       };
     };
+
+    //window.indexedDB > DB.open() 
+    // >> initialise = onupgradedneeded() > db.createObjectStore > createIndex() > put() 
+    // - Write
+    // >> onsuccess > transaction context > find object store > openCursor() > cursor.onsuccess() > event.target as IDBRequest >  cursor.value > cursor.continue > end
+    // - Read
+
+
 
     return () => {
       console.log("clean up");
