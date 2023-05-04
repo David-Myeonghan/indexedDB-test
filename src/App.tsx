cbb98e62bfd5d7a21889ebec84954f26c1c9b406
davidRyu <david.ryu@imagoworks.ai>
2023-05-04 17:14:08 +0900

    feat: basic call

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import './App.css';
-import IndexedDB from './indexedDB';
-
-
+import IndexedDB from './IndexedDB';
 
 const imgs = [
   {title: 'test', src:'https://images.unsplash.com/photo-1682957376808-dcb27d61f95e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80'}
--- src/IndexedDB.tsx
+++ src/IndexedDB.tsx
@@ -1,16 +1,81 @@
 import { useEffect } from "react";
 
-
 const IndexedDB = () => {
-    console.log('test')
+  console.log("test");
 
-    useEffect(() => {
-        console.log('useEffect')
-        return () => {
-            console.log('clean up')
+  useEffect(() => {
+    console.log("useEffect");
+    console.log(window.indexedDB);
+    if (!window.indexedDB) {
+      window.alert(
+        "Your browser doesnt support a stable version of IndexedDB. Such and such feature will not be available"
+      );
+    }
+
+    const name = "IndexedDB Name"; // db name
+    const version = 1;
+    let db = null;
+
+    // IDBOpenDBRequest
+    const request = indexedDB.open(name, version);
+    console.log(request);
+
+    // trigger when db is loaded with bigger version than existing number.
+    request.onupgradeneeded = function (event) {
+      // IDBDatabase
+      db = request.result;
+      console.log(db);
+
+      const key = "id";
+      const name = "store name"; // store name
+
+      // IDBObjectStore
+      const store = db.createObjectStore(name, { keyPath: key });
+      console.log(store);
+
+      const indexName = "by_name"; // table name
+      const keyPath = "name";
+
+      // IDBIndex
+      const index = store.createIndex(indexName, keyPath);
+      console.log(index);
+
+      const obj = { [key]: 1, [keyPath]: "name" };
+      store.put(obj);
+    };
+    request.onsuccess = function (event) {
+      db = request.result;
+      console.log(db);
+
+      // IDBTransaction
+      const transaction = db.transaction(["store name"], "readonly");
+
+      // IDBObjectStore
+      const objectStore = transaction.objectStore("store name");
+
+      // IDBRequest
+      const cursor = objectStore.openCursor();
+      cursor.onsuccess = function (event: Event) {
+        // IDBCursorWithValue
+        console.log(event);
+        const target = event.target as IDBRequest;
+        const cursor = target.result ?? "";
+        console.log(cursor)
+        if (cursor) {
+            // { id: 1, name: 'name' }
+            console.log(cursor.value);
+            cursor.continue();
+        } else {
+            console.log('end');
         }
-    }, [])
-    return 'test'
-}
+      };
+    };
+
+    return () => {
+      console.log("clean up");
+    };
+  }, []);
+  return "test";
+};
 
 export default IndexedDB;
